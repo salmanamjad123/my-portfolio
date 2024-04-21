42ab9d88476fe173ceb3a47b6fdf52de831a8422
innovative mojo <developer@innovativemojo.com>
2024-04-21 11:27:33 +0500

    first commit

--- components/about/AboutClients.jsx
+++ components/about/AboutClients.jsx
@@ -7,7 +7,7 @@ function AboutClients() {
 	const [clients, setClients] = useState(clientsData);
 	return (
 		<div className="mt-10 sm:mt-20">
-			<p className="font-general-medium text-2xl sm:text-3xl  text-center text-primary-dark dark:text-primary-light">
+			{/* <p className="font-general-medium text-2xl sm:text-3xl  text-center text-primary-dark dark:text-primary-light">
 				{clientsHeading}
 			</p>
 			<div className="grid grid-cols-2 sm:grid-cols-4 mt-10 sm:mt-14 gap-2">
@@ -18,7 +18,7 @@ function AboutClients() {
 						key={client.id}
 					/>
 				))}
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- components/about/AboutMeBio.jsx
+++ components/about/AboutMeBio.jsx
@@ -8,7 +8,7 @@ function AboutMeBio() {
 		<div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
 			<div className="w-full sm:w-1/4 mb-7 sm:mb-0">
 				<Image
-					src="/images/profile.jpeg"
+					src="/images/myimage.jpg"
 					width={200}
 					height={200}
 					className="rounded-lg"
@@ -17,14 +17,19 @@ function AboutMeBio() {
 			</div>
 
 			<div className="font-general-regular w-full sm:w-3/4 text-left">
-				{aboutMe.map((bio) => (
+				{/* {aboutMe.map((bio) => ( */}
 					<p
 						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
-						key={bio.id}
+						// key={bio.id}
 					>
-						{bio.bio}
+						{/* {bio.bio} */}
+						Hey, I'm Salman Amjad—a seasoned MERN Stack developer specializing in React.js, Next.js, and Node.js, Express.js,. With over 3 years of hands-on experience, I craft dynamic interfaces, integrating technologies like Firebase, Tailwind CSS, Bootstrap, and third-party APIs. Particularly adept at building admin panels and websites, I excel in creating clean, scalable code for seamless user experiences.
 					</p>
-				))}
+					<p
+						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+					>
+						Let's collaborate—I bring precision, innovation, and a commitment to pushing the boundaries of web development to help you achieve your goals.	</p>
+				{/* ))} */}
 			</div>
 		</div>
 	);
